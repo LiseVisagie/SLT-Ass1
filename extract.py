9d6b60fc1326734e47d9d506208bd4184f305a17
Lise <lisevisagie7@gmail.com>
2026-06-11 09:48:53 +0200

    add features

--- extract.py
+++ extract.py
@@ -9,20 +9,22 @@ def fetch_data(start_date="2025-01-01"):
     gold = yf.download("GC=F", start=start_date)
     sp500 = yf.download("^GSPC", start=start_date)
     vix = yf.download("^VIX", start=start_date)
-    
+    dxy = yf.download("DX-Y.NYB", start=start_date)
+    brent = yf.download("BZ=F", start=start_date)
+
     df = pd.DataFrame()
     df["Silver_USD"] = silver["Close"]
     df["USDZAR"] = fx["Close"]
     df["Gold_USD"] = gold["Close"]
     df["SP500"] = sp500["Close"]
     df["VIX"] = vix["Close"]
+    df["DXY"] = dxy["Close"]
+    df["Brent"] = brent["Close"]
     
     macro = pd.DataFrame()
- 
     macro["US_Interest_Rate"] = pdr.DataReader("FEDFUNDS", "fred", start_date)
     macro["US_10Y_Yield"] = pdr.DataReader("DGS10", "fred", start_date)
 
-    
 
     df = df.join(macro, how="left")
     df = df.ffill()
