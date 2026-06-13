c5cc6196309a54cf4be1b5f11e36d330f54e7dd9
Lise <lisevisagie7@gmail.com>
2026-06-13 13:44:05 +0200

    Finally working GARCH

--- extract.py
+++ extract.py
@@ -1,3 +1,4 @@
+import requests
 import yfinance as yf
 import pandas as pd
 from pandas_datareader import data as pdr
@@ -21,9 +22,36 @@ def fetch_data(start_date="2025-01-01"):
     df["DXY"] = dxy["Close"]
     df["Brent"] = brent["Close"]
     
+
+
+    API_KEY = "3b99aac88e43596ece17437838d3bbaa"
+
+    def get_fred_series(series_id, start_date="2025-01-01"):
+        
+        url = (
+            "https://api.stlouisfed.org/fred/series/observations"
+            f"?series_id={series_id}"
+            f"&api_key={API_KEY}"
+            "&file_type=json"
+        )
+
+        r = requests.get(url, timeout=60)
+        r.raise_for_status()
+
+        data = r.json()["observations"]
+
+        df = pd.DataFrame(data)[["date", "value"]]
+
+        df["date"] = pd.to_datetime(df["date"])
+        df["value"] = pd.to_numeric(df["value"], errors="coerce")
+
+        df = df[df["date"] >= start_date]
+
+        return df.set_index("date")["value"]
+
     macro = pd.DataFrame()
-    macro["US_Interest_Rate"] = pdr.DataReader("FEDFUNDS", "fred", start_date)
-    macro["US_10Y_Yield"] = pdr.DataReader("DGS10", "fred", start_date)
+    macro["US_Interest_Rate"] = get_fred_series("FEDFUNDS", start_date)
+    macro["US_10Y_Yield"] = get_fred_series("DGS10", start_date)
 
 
     df = df.join(macro, how="left")
--- transform.py
+++ transform.py
@@ -16,30 +16,32 @@ def add_features(df):
     df["gold_returns"] = np.log(df["Gold_USD"] / df["Gold_USD"].shift(1))
     df["sp500_returns"] = np.log(df["SP500"] / df["SP500"].shift(1))
     df["fx_returns"] = np.log(df["USDZAR"] / df["USDZAR"].shift(1))
+    df["brent_returns"] = np.log(df["Brent"] / df["Brent"].shift(1))
+    df["dxy_returns"] = np.log(df["DXY"] / df["DXY"].shift(1))
     df["vix_changes"] = df["VIX"].pct_change()
     df["gold_silver_ratio"] = df["Gold_USD"] / df["Silver_USD"]
     
     df["gold_fx_interaction"] = df["gold_returns"] * df["fx_returns"]
 
-    df["target"] = df["log_returns"].shift(-1)
+    df["target_return"] = df["log_returns"].shift(-1)
     
 
 
     df["volatility_7"] = df["log_returns"].rolling(7).std()
     df["volatility_14"] = df["log_returns"].rolling(14).std()
+    df["volatility_21"] = (df["log_returns"].rolling(21).std())
+    df["volatility_63"] = (df["log_returns"].rolling(63).std())
     df["volatility_ratio"] = df["volatility_7"] / df["volatility_14"]
-    
-    df["realized_vol_21"] = (df["log_returns"].rolling(21).std())
-    df["realized_vol_63"] = (df["log_returns"].rolling(63).std())
     
     df["gold_vol_21"] = (df["gold_returns"].rolling(21).std())
     df["sp500_vol_21"] = (df["sp500_returns"].rolling(21).std())
-    df["fx_volatility"] = df["fx_returns"].rolling(7).std()
+    df["fx_vol_21"] = df["fx_returns"].rolling(21).std()
+    df["brent_vol_21"] = (df["brent_returns"].rolling(21).std())
 
     for lag in range(1, 5):
         df[f"abs_return_lag_{lag}"] = abs(df["log_returns"].shift(lag))
 
-    df["target_vol"] = df["log_returns"].shift(-1)**2
+    df["target_vol"] = (df["log_returns"].rolling(5).std().shift(-1))
 
     df = df.replace([np.inf, -np.inf], np.nan)
     df = df.dropna()
